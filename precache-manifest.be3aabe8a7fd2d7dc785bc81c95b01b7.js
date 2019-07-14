4aa7c78af89341379f938ad625fb9de0f05f66c6
Prateek Srivastava <prateeksrt2009@gmail.com>
2019-07-14 21:26:24 +0700

    Updates

---
+++ precache-manifest.be3aabe8a7fd2d7dc785bc81c95b01b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54583b40d5e5e37e01c153214e4a17f",
+    "url": "/bookmark/index.html"
+  },
+  {
+    "revision": "ea064223c8efd7a1d49e",
+    "url": "/bookmark/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "4461314a0857b20b6362",
+    "url": "/bookmark/static/css/main.c9ba0cfe.chunk.css"
+  },
+  {
+    "revision": "ea064223c8efd7a1d49e",
+    "url": "/bookmark/static/js/2.d59eeaa2.chunk.js"
+  },
+  {
+    "revision": "4461314a0857b20b6362",
+    "url": "/bookmark/static/js/main.8915551f.chunk.js"
+  },
+  {
+    "revision": "5357d892f55cf939e62c",
+    "url": "/bookmark/static/js/runtime~main.1071e476.js"
+  }
+]);
